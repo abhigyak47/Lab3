eee093d8369062eadf5bf2bb6031a6747d81b34d
abhigyak47 <124084882+abhigyak47@users.noreply.github.com>
2023-04-30 20:43:31 -0400

    Update chatEntry.h

--- chatEntry.h
+++ chatEntry.h
@@ -2,23 +2,23 @@
 #include <string>
 using namespace std;
 
-#ifndef PIZZAENTRY_H
-#define PIZZAENTRY_H
+#ifndef CHATENTRY_H
+#define CHATENTRY_H
 
-class pizzaEntry {
+class chatEntry {
 public:
-     pizzaEntry();
-     pizzaEntry(sql::SQLString ID, sql::SQLString Name, sql::SQLString Address, sql::SQLString Rating);
+     chatEntry();
+     chatEntry(sql::SQLString ID, sql::SQLString user, sql::SQLString email, sql::SQLString pass);
      string text();
      string json();
      
   string ID;
-  string Name;
-  string Address;
-  string Rating;;
+  string user;
+  string email;
+  string pass;;
 
 private:
 
 };
 
-#endif /* PIZZAENTRY_H */
+#endif /* CHATENTRY_H */
